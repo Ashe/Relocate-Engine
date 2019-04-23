d1d19f4d61c110fe55892e6a8920b61e9edf46fb
Ashley Smith <ashley@aas.sh>
2019-04-23 05:32:23 +0100

    Cleaned up the gameconfig.lua file and some old debugs

--- GameConfig.lua
+++ GameConfig.lua
@@ -7,12 +7,5 @@ print("Running GameConfig.lua")
 -- Simply enable debug from the very beginning
 Game.debug = false
 
--- Load some other lua scripts
--- @NOTE: This is not the way to do this, this is just
--- to expose spells to global scope before
--- resource manager has the functionality to load spells
-_, _, leftSpell = dofile("Assets/Spells/LaunchBox.lua")
-_, _, rightSpell = dofile("Assets/Spells/Levitate.lua")
-
 local attemptMultiThread = true
 return attemptMultiThread
